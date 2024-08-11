58a582934fb83dcdf8bb5a5fa870dbd729219495
argonmining <ashton@onargon.com>
2024-08-11 08:58:41 -0500

    testing new prom gauges

--- src/pool/database/index.ts
+++ src/pool/database/index.ts
@@ -26,31 +26,31 @@ export default class Database {
 
   async addBalance(minerId: string, wallet: string, balance: bigint) {
     const key = `${minerId}_${wallet}`;
-    
+
     await this.client.query('BEGIN');
     try {
       // Update miners_balance table
       const res = await this.client.query('SELECT balance FROM miners_balance WHERE id = $1', [key]);
       let minerBalance = res.rows[0] ? BigInt(res.rows[0].balance) : 0n;
       minerBalance += balance;
-  
+
       await this.client.query('INSERT INTO miners_balance (id, miner_id, wallet, balance) VALUES ($1, $2, $3, $4) ON CONFLICT (id) DO UPDATE SET balance = EXCLUDED.balance', [
         key,
         minerId,
         wallet,
         minerBalance,
       ]);
-  
+
       // Update wallet_total table
       const resTotal = await this.client.query('SELECT total FROM wallet_total WHERE address = $1', [wallet]);
       let walletTotal = resTotal.rows[0] ? BigInt(resTotal.rows[0].total) : 0n;
       walletTotal += balance;
-  
+
       await this.client.query('INSERT INTO wallet_total (address, total) VALUES ($1, $2) ON CONFLICT (address) DO UPDATE SET total = EXCLUDED.total', [
         wallet,
         walletTotal,
       ]);
-  
+
       await this.client.query('COMMIT');
       return true;
     } catch (e) {
@@ -58,12 +58,11 @@ export default class Database {
       throw e;
     }
   }
-  
 
   async resetBalanceByAddress(wallet: string) {
     await this.client.query('UPDATE miners_balance SET balance = $1 WHERE wallet = $2', [0n, wallet]);
   }
-  
+
   async getAllBalances() {
     const res = await this.client.query('SELECT miner_id, wallet, balance FROM miners_balance');
     return res.rows.map((row: MinerBalanceRow) => ({
@@ -81,5 +80,4 @@ export default class Database {
     }
     return { balance: BigInt(res.rows[0].balance) };
   }
-  
 }

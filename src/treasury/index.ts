34b42711fbf6083fa737b6ca94b4b123365507df
parth-kr <parth.p@knackroot.com>
2024-12-26 19:01:19 +0530

    Check for isCoinbase utxoEntries

--- src/treasury/index.ts
+++ src/treasury/index.ts
@@ -2,17 +2,25 @@ import { EventEmitter } from 'events'
 import Monitoring from '../monitoring';
 import { PrivateKey, UtxoProcessor, UtxoContext, type RpcClient } from "../../wasm/kaspa"
 
+const startTime = BigInt(Date.now())
+
+UtxoProcessor.setCoinbaseTransactionMaturityDAA('mainnet', 200n)
+UtxoProcessor.setCoinbaseTransactionMaturityDAA('testnet-10', 200n)
+UtxoProcessor.setCoinbaseTransactionMaturityDAA('testnet-11', 2000n)
+
 export default class Treasury extends EventEmitter {
   privateKey: PrivateKey
   address: string
   processor: UtxoProcessor
   context: UtxoContext
   fee: number
+  rpc: RpcClient
   private monitoring: Monitoring;
 
   constructor(rpc: RpcClient, networkId: string, privateKey: string, fee: number) {
     super()
 
+    this.rpc = rpc  
     this.privateKey = new PrivateKey(privateKey)
     this.address = (this.privateKey.toAddress(networkId)).toString()
     this.processor = new UtxoProcessor({ rpc, networkId })
@@ -31,7 +39,17 @@ export default class Treasury extends EventEmitter {
       await this.context.trackAddresses([this.address])
     })
 
-    this.processor.addEventListener('maturity', (e) => {
+    this.processor.addEventListener('maturity', async (e) => {
+      // @ts-ignore
+      if (!e?.data?.data?.utxoEntries?.some(element => element?.isCoinbase)) return
+
+      const { timestamps } = await this.rpc.getDaaScoreTimestampEstimate({
+        daaScores: [ e.data.blockDaaScore ]
+      })
+      if (timestamps[0] < startTime) {
+        return
+      }
+
       // @ts-ignore
       const reward = e.data.value
       this.monitoring.log(`Treasury: Maturity event received. Reward: ${reward}, Event timestamp: ${Date.now()}`);
